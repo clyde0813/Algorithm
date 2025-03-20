f08afd544021c09b690637451cca9c313a5728ba
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-03-20 18:48:21 +0900

    [level 2] Title: 짝지어 제거하기, Time: 25.75 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/README.md
+++ 프로그래머스/2/12973. 짝지어 제거하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 짝지어 제거하기 - 12973 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12973) 
 
 ### 성능 요약
 
-메모리: 66.5 MB, 시간: 62.76 ms
+메모리: 61.3 MB, 시간: 25.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 20일 17:03:05
+2025년 03월 20일 18:48:19
 
 ### 문제 설명
 
--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -2,10 +2,14 @@ import java.util.*;
 
 class Solution
 {
-    public int solution(String s)
+    public int solution(String s){
+        return removePairsWithArray(s);
+    }
+    
+    // ArrayDeque 사용
+    public int removePairsWithDeque(String s)
     {
         Deque<Character> stack = new ArrayDeque<>();
-        
         for(char c : s.toCharArray()){
             if(!stack.isEmpty() && stack.peek() == c){
                 stack.pop();
@@ -14,5 +18,19 @@ class Solution
             }
         }
         return stack.isEmpty() ? 1 : 0;
+    }
+    
+    // 배열 사용
+    public int removePairsWithArray(String s){
+        char[] stack = new char[s.length()];
+        int idx = -1;
+        for(char c : s.toCharArray()){
+            if(idx >= 0 && stack[idx] == c){
+                idx--;
+            } else {
+                stack[++idx] = c;
+            }
+        }
+        return idx == -1 ? 1 : 0;
     }
 }
