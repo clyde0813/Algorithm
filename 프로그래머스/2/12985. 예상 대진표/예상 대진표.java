79e1d2d34335e7810c37e67f32322c6aadd7d911
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-13 19:36:27 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int n, int a, int b)
+    {
+        int answer = 1;
+        while(true){
+            if((int) (a+1)/2 == (int) (b+1)/2) return answer;
+            a = (int) (a + 1) / 2;
+            b = (int) (b + 1) / 2;
+            answer++;
+        }
+    }
+}
