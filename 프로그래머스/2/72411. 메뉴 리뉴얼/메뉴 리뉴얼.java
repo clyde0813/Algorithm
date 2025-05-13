c7d4d669db0e4941d685c252dc61afae2869da09
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-13 18:40:35 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 11.52 ms, Memory: 96.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -5,7 +5,6 @@ class Solution {
         List<String> answer = new ArrayList<>();
         for(int c : course){
             Map<String, Integer> hashmap = new HashMap<>();
-            int maxOrder = 0;
             for(String order : orders){
                 if(order.length() >= c){
                     char[] orderCharArray = order.toCharArray();
@@ -13,14 +12,13 @@ class Solution {
                     combination(orderCharArray, new StringBuilder(), 0, c, hashmap);
                 }
             }
-            for(Map.Entry<String, Integer> entry : hashmap.entrySet()){
-                if(entry.getValue()>=2){
-                    maxOrder = entry.getValue() > maxOrder ? entry.getValue() : maxOrder;
-                };
-            }
-            for(Map.Entry<String, Integer> entry : hashmap.entrySet()){
-                if(entry.getValue() == maxOrder) answer.add(entry.getKey());
-            }
+            hashmap.values().stream()
+                .filter(o -> o >= 2)
+                .max(Comparator.comparingInt(o -> o))
+                .ifPresent(maxOrder -> hashmap.entrySet().stream()
+                           .filter(entry -> entry.getValue() == maxOrder)
+                           .forEach(entry -> answer.add(entry.getKey()))
+                );
         }
         Collections.sort(answer);
         return answer.toArray(String[]::new);
