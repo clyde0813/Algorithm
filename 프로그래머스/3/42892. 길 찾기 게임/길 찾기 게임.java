d80f5ea08abdc53ff4f3d254852cabbb01a70b1c
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-22 20:21:27 +0900

    [level 3] Title: 길 찾기 게임, Time: 73.51 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,68 +1,51 @@
 import java.util.*;
 
 class Solution {
+    static class Node{
+        public int x, y, idx;
+        public Node left, right;
+        
+        public Node(int x, int y, int idx){
+            this.x = x;
+            this.y = y;
+            this.idx = idx;
+        }
+    }
+    
     public int[][] solution(int[][] nodeinfo) {
-        int[][] answer = new int[nodeinfo[0].length][nodeinfo.length];
-        List<int[]> nodeList = new ArrayList<>(Arrays.asList(nodeinfo));
-        Map<Integer, Integer> xIndexMap = new HashMap<>();
-        Map<Integer, List<Integer>> xLevelMap = new HashMap<>();
+        Node[] nodes = new Node[nodeinfo.length];
         
         for(int i=0; i<nodeinfo.length; i++){
-            xIndexMap.put(nodeinfo[i][0], i+1);
+            nodes[i] = new Node(nodeinfo[i][0], nodeinfo[i][1], i+1);
         }
         
-        Collections.sort(nodeList, (o1, o2) -> o2[1] - o1[1]);
+        Arrays.sort(nodes, (o1, o2) -> o2.y - o1.y);
         
-        for(int[] nodes : nodeList){
-            xLevelMap.computeIfAbsent(nodes[1], o -> new ArrayList<>()).add(nodes[0]);
+        Node root = nodes[0];
+        
+        for(int i=1; i<nodes.length; i++){
+            insertNode(root, nodes[i]);
         }
         
-        List<Integer> keySet = new ArrayList<>(xLevelMap.keySet());
-        
-        Collections.sort(keySet, Comparator.reverseOrder());
-        
-        Node node = new Node(nodeList.get(0)[0]);
-        Node root = node;
-        
-        for(int i=1; i<keySet.size(); i++){
-            Integer key = keySet.get(i);
-            for(Integer v : xLevelMap.get(key)){
-                node = root;
-                while(true){
-                    if(node.value<v&&node.right!=null){
-                        node = node.right;
-                    } 
-                    else if(node.value<v&&node.right==null){
-                        node.right = new Node(v);
-                        break;
-                    }
-                    else if(node.value>v&&node.left!=null){
-                        node = node.left;
-                    }
-                    else if(node.value>v&&node.left==null){
-                        node.left = new Node(v);
-                        break;
-                    }
-                }
-            }
+        return new int[][]{
+            preorder(root).stream().mapToInt(i -> i).toArray(),
+            postorder(root).stream().mapToInt(i -> i).toArray()
+        };
+    }
+    
+    private void insertNode(Node parent, Node child){
+        if(parent.x > child.x){
+            if(parent.left==null) parent.left = child;
+            else insertNode(parent.left, child);
+        } else {
+            if(parent.right==null) parent.right = child;
+            else insertNode(parent.right, child);
         }
-        List<Integer> pre = preorder(root);
-        List<Integer> post = postorder(root);
-        
-        for(int i=0; i<pre.size(); i++){
-            answer[0][i] = xIndexMap.get(pre.get(i));
-        }
-        
-        for(int i=0; i<post.size(); i++){
-            answer[1][i] = xIndexMap.get(post.get(i));
-        }
-        
-        return answer;
     }
     
     private List<Integer> preorder(Node node){
         List<Integer> order = new ArrayList<>();
-        order.add(node.value);
+        order.add(node.idx);
         if(node.left!=null) order.addAll(preorder(node.left));
         if(node.right!=null) order.addAll(preorder(node.right));
         return order;
@@ -72,19 +55,7 @@ class Solution {
         List<Integer> order = new ArrayList<>();
         if(node.left!=null) order.addAll(postorder(node.left));
         if(node.right!=null) order.addAll(postorder(node.right));
-        order.add(node.value);
+        order.add(node.idx);
         return order;
-    }
-    
-    static class Node{
-        public int value;
-        public Node left;
-        public Node right;
-        
-        public Node(int value){
-            this.value = value;
-            this.left = null;
-            this.right = null;
-        }
     }
 }
