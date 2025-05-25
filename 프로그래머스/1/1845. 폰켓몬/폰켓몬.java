32fe1b782ecd877a87ae2aeee76a39f2949a6d8c
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-25 19:02:37 +0900

    [level 1] Title: 폰켓몬, Time: 4.56 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -6,6 +6,6 @@ class Solution {
         for(int num : nums){
             numSet.add(num);
         }        
-        return ((nums.length/2) < numSet.size()) ? (nums.length/2) : numSet.size();
+        return Math.min((nums.length/2), numSet.size());
     }
 }
