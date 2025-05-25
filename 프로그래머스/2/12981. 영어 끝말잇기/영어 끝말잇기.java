8b8ec1ed2ea4a2a143a34497d438fe7d2898afff
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-25 20:27:01 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.26 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int n, String[] words) {
+        Set<String> wordSet = new HashSet<>();
+        
+        for(int i=0; i<words.length; i++){
+            String word = words[i];
+            if(i>0 && word.charAt(0)!=words[i-1].charAt(words[i-1].length()-1)){
+                System.out.println(i);
+                return new int[]{(i%n+1), (i/n+1)};
+            }
+            else if(wordSet.contains(word)){
+                return new int[]{(i%n+1), (i/n+1)};
+            }
+            wordSet.add(word);
+        }
+
+        return new int[]{0,0};
+    }
+}
