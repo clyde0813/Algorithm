d65629eaaf3f00635374cbeea3770bad2c3330c7
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-26 22:43:55 +0900

    [level 1] Title: 기사단원의 무기, Time: 11.26 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,15 +1,18 @@
 class Solution {
     public int solution(int number, int limit, int power) {
-        int answer = 1;
-        for(int i=2; i<number+1; i++){
-            int kPower = 0;
-            for(int j=1; j*j<i+1; j++){
-                if(j*j==i) kPower += 1;
-                else if(i%j==0) kPower += 2;
+        int[] divisorCount = new int[number + 1];
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = i; j <= number; j += i) {
+                divisorCount[j]++;
             }
-            if(kPower>limit) answer += power;
-            else answer += kPower;
         }
+
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            answer += (divisorCount[i] > limit) ? power : divisorCount[i];
+        }
+
         return answer;
     }
 }
