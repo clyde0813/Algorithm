aeb440b1a238fae97fd2142177077b56c4f20d3a
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-31 20:41:41 +0900

    Java / 0.08307692307692308ms

--- PROGRAMMERS/43162_네트워크.java
+++ PROGRAMMERS/43162_네트워크.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] computers) {
-        int[] parents = new int[n];
-        for(int i=0; i<n; i++) parents[i] = i;
+        boolean[] visited = new boolean[n];
         
+        int answer = 0;
         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(computers[i][j]==0) continue;
-                if(find(parents, i)==find(parents, j)) continue;
-                union(parents, i, j);
+            if(visited[i]==false){
+                dfs(computers, visited, i, n);
+                answer++;
             }
         }
         
-        Set<Integer> set = new HashSet<>();
-        for(int p : parents) set.add(find(parents, p));
+        return answer;
+    }
+    
+    private void dfs(int[][] computers, boolean[] visited, int target, int n){
+        if(visited[target]==true) return;
         
-        return set.size();
-    }
-    
-    private int find(int[] parents, int target){
-        if(parents[target]!=target) parents[target] = find(parents, parents[target]);
-        return parents[target];
-    }
-    
-    private void union(int[] parents, int target, int child){
-        parents[find(parents, child)] = find(parents, target);
+        visited[target] = true;
+        for(int i=0; i<n; i++){
+            if(computers[target][i]==1){
+                dfs(computers, visited, i, n);
+            }
+        }
     }
 }
