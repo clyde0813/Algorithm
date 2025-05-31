727cdfdd11009475f1bd5271d51e2cc8fe455e3a
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-31 12:34:14 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.79 ms, Memory: 56.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,47 +1,31 @@
 import java.util.*;
 
 class Solution {
-    private int answer = 1;
-    private int[][] maps;
-    private int[][] visited;
-    private int n, m;
-    private int[][] dirs = {
-        {0, 1},
-        {0, -1},
-        {1, 0},
-        {-1, 0}
-    };
-    
     public int solution(int[][] maps) {
-        this.maps = maps;
-        this.n = maps[0].length;
-        this.m = maps.length;
-        this.visited = new int[m][n];
-        bfs(0, 0, 1);
-        return (maps[m-1][n-1]==1) ? -1 : maps[m-1][n-1];
-    }
-    
-    private void bfs(int y, int x, int value){
-        Deque<int[]> deque = new ArrayDeque<>();
-        deque.addFirst(new int[]{y, x, value});
-        while(!deque.isEmpty()){
-            int[] position = deque.pollLast();
-            y = position[0];
-            x = position[1];
-            value = position[2];
+        int n = maps[0].length;
+        int m = maps.length;
+        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+        boolean[][] visited = new boolean[m][n];
+        
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0,0,1});
+        visited[0][0] = true;
+        
+        while(!queue.isEmpty()){
+            int[] curr = queue.poll();
+            int y = curr[0], x = curr[1], v = curr[2];
             
-            if(!isInBound(y, x) || maps[y][x]==0 || visited[y][x]==1) continue;
-            
-            visited[y][x] = 1;
-            maps[y][x] = value++;
+            if(y==m-1&&x==n-1) return v;
             
             for(int[] dir : dirs){
-                deque.addFirst(new int[]{y+dir[0], x+dir[1], value});
+                int ny = y+dir[0], nx = x+dir[1];
+                if(m<=ny||ny<0||n<=nx||nx<0) continue;
+                if(visited[ny][nx]||maps[ny][nx]==0) continue;
+                visited[ny][nx] = true;
+                queue.offer(new int[]{ny, nx, v+1});   
             }
         }
-    }
-    
-    private boolean isInBound(int y, int x){
-        return m>y && y>=0 && n>x && x>=0;
+        
+        return -1;
     }
 }
