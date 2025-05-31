22116a50dfc4dcd287c4e1d0f894028f8739db48
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-05-31 13:08:11 +0900

    [level 3] Title: 섬 연결하기, Time: 0.61 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,39 +1,35 @@
 import java.util.*;
 
 class Solution {
-    private int[] parent;
     public int solution(int n, int[][] costs) {
-        this.parent = new int[n];
-        for(int i=0; i<n; i++) parent[i] = i;
+        if(n==1) return 0;
         
-        int totalCost = 0;
-        int edgesUsed = 0;
+        int[] parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
         
         Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
         
-        for(int[] cost : costs){
-            int x = cost[0], y = cost[1], c = cost[2];
-            if(find(x) != find(y)){
-                union(x, y);
-                totalCost += c;
-                edgesUsed++;
-                if(edgesUsed == n-1) break;
-            }
-        }
+        int answer = 0;
+        int edges = 0;
         
-        return totalCost;
-    }
-    
-    private int find(int x){
-        if(parent[x] != x){
-            parent[x] = find(parent[x]);
+        for(int[] cost : costs){
+            if(find(parent, cost[0]) == find(parent, cost[1])) continue;
+            
+            union(parent, cost[0], cost[1]);
+            answer += cost[2];
+            if(edges++ == n-1) break;
         }
-        return parent[x];
+        return answer;
     }
     
-    private void union(int x, int y){
-        int xParent = find(x);
-        int yParent = find(y);
-        parent[yParent] = xParent;
+    private int find(int[] parent, int target){
+        if(parent[target] != target){
+            parent[target] = find(parent, parent[target]);
+        }
+        return parent[target];
+    }
+    
+    private void union(int[] parent, int target, int child){
+        parent[find(parent, child)] = find(parent, target);
     }
 }
