d9aa931e8e851f65fc8841cd3098bfa5ff99a1e9
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-01 08:54:02 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.07 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -2,11 +2,10 @@ import java.util.*;
 
 class Solution {
     public int solution(int a, int b) {
-        return Math.max(combine(a,b), combine(b,a));
+        return Math.max(combine(a, b), combine(b, a));
     }
     
     private int combine(int a, int b){
-        StringBuilder sb = new StringBuilder();
-        return Integer.parseInt(sb.append(a).append(b).toString());
-    }
+        return (int) (a * Math.pow(10, (int) (Math.log10(b)+1)) + b);
+    } 
 }
