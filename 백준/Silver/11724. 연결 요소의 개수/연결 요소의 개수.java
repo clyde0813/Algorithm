b304e1ced4b310c6617471c27015251747b9f253
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-01 12:41:47 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 440 ms, Memory: 113396 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int nodeCount = Integer.parseInt(st.nextToken());
+        int edgeCount = Integer.parseInt(st.nextToken());
+        
+        int[] parent = new int[nodeCount];
+        for(int i=0; i<nodeCount; i++) parent[i] = i;
+        
+        for(int i=0; i<edgeCount; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken()) - 1;
+            union(parent, start, end);
+        }
+        
+        Set<Integer> answerSet = new HashSet<>();
+        for(int i=0; i<parent.length; i++){
+            answerSet.add(find(parent, i));
+        }
+        
+        System.out.println(answerSet.size());
+    }
+    
+    private static int find(int[] parent, int target){
+        if(parent[target]!=target){
+            parent[target] = find(parent, parent[target]);
+        }
+        return parent[target];
+    }
+    
+    private static void union(int[] parent, int target, int child){
+        parent[find(parent, child)] = find(parent, target);
+    }
+}
