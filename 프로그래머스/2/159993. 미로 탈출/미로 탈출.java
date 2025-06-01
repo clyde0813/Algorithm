c37e44b9e565c32bbe1a0a5c07f6a908f85e7d0f
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-01 16:35:22 +0900

    [level 2] Title: 미로 탈출, Time: 7.49 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -1,7 +1,6 @@
 import java.util.*;
 
 class Solution {
-    private int[][] map;
     private int n, m;
     private int[][] dirs = {{1,0}, {-1,0}, {0,1}, {0,-1}};
     
@@ -12,27 +11,25 @@ class Solution {
         
         this.n = maps.length;
         this.m = maps[0].length();
-        this.map = new int[n][m];
         
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
-                int value = 0;
                 int chr = maps[i].charAt(j);
-                if(chr == 'X') value = 1;
-                else if(chr == 'S') start = new int[]{i, j};
-                else if(chr == 'L') lever = new int[]{i,j};
-                else if(chr == 'E') exit = new int[]{i, j};
-                this.map[i][j] = value;
+                if(chr=='S') start = new int[]{i, j};
+                if(chr=='L') lever = new int[]{i,j};
+                if(chr=='E') exit = new int[]{i, j};
             }
         }
-        int startToLever = bfs(start, lever);
-        int leverToExit = bfs(lever, exit);
+        
+        int startToLever = bfs(maps, start, lever);
+        int leverToExit = bfs(maps, lever, exit);
+        
         if(startToLever==-1 || leverToExit==-1) return -1;
         
         return startToLever + leverToExit;
     }
     
-    private int bfs(int[] start, int[] end){
+    private int bfs(String[] maps, int[] start, int[] end){
         boolean[][] visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
         
@@ -43,14 +40,14 @@ class Solution {
             int[] q = queue.poll();
             int y = q[0], x = q[1], v = q[2];
 
-            if(y==end[0]&&x==end[1]) return v;
+            if(y==end[0] && x==end[1]) return v;
             
             for(int[] dir : dirs){
                 int dy = y+dir[0];
                 int dx = x+dir[1];
                 
                 if(!isInBound(dy, dx)) continue;
-                if(map[dy][dx]==1) continue;
+                if(maps[dy].charAt(dx)=='X') continue;
                 if(visited[dy][dx]) continue;
                 
                 visited[dy][dx] = true;
@@ -62,6 +59,6 @@ class Solution {
     }
     
     private boolean isInBound(int y, int x){
-        return n>y&&y>=0&&m>x&&x>=0;
+        return n>y && y>=0 && m>x && x>=0;
     }
 }
