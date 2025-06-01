0cdabbd3e8ba7eb5edd2f4c59afb201ebea414d4
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-01 20:17:39 +0900

    [level 2] Title: 배달, Time: 6.70 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -7,32 +7,31 @@ class Solution {
         for(int i=0; i<N; i++) graph[i] = new ArrayList<>();
         
         for(int[] r : road){
-            graph[r[0]-1].add(new int[]{r[0]-1, r[1]-1, r[2]});
-            graph[r[1]-1].add(new int[]{r[1]-1, r[0]-1, r[2]});
+            graph[r[0]-1].add(new int[]{r[1]-1, r[2]});
+            graph[r[1]-1].add(new int[]{r[0]-1, r[2]});
         }
         
         int[] dist = new int[N];
-        for(int i=0; i<N; i++) dist[i] = Integer.MAX_VALUE;
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        
         dist[0] = 0;
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        pq.offer(new int[]{0, 0, 0});
+        pq.offer(new int[]{0, 0});
         
         while(!pq.isEmpty()){
             int[] curr = pq.poll();
-            int s = curr[0], e = curr[1], c = curr[2];
-            
-            for(int[] next : graph[e]){
-                if(dist[e] + next[2] > dist[next[1]]) continue;
+            int cur = curr[0];
+            for(int[] next : graph[cur]){
+                if(dist[cur] + next[1] > dist[next[0]]) continue;
                 
-                dist[next[1]] = dist[e] + next[2];
+                dist[next[0]] = dist[cur] + next[1]; 
                 pq.add(next);
             }
         }
         
         int answer = 0;
         for(int d : dist){
-            System.out.print(d+" ");
             if(d<=K) answer++;
         }
         
