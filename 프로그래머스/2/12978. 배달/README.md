7e59c35cb83946cbf0524131da761c10dfb54365
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-04 19:09:49 +0900

    [level 2] Title: 배달, Time: 3.52 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/README.md
+++ 프로그래머스/2/12978. 배달/README.md
@@ -1,10 +1,10 @@
 # [level 2] 배달 - 12978 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12978) 
 
 ### 성능 요약
 
-메모리: 88.8 MB, 시간: 7.31 ms
+메모리: 86.2 MB, 시간: 3.52 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 01일 20:21:07
+2025년 06월 04일 19:09:45
 
 ### 문제 설명
 
--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,37 +2,37 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
+        if(N==1) return 1;
+        
         List<int[]>[] graph = new ArrayList[N];
-        
         for(int i=0; i<N; i++) graph[i] = new ArrayList<>();
-        
         for(int[] r : road){
             graph[r[0]-1].add(new int[]{r[1]-1, r[2]});
             graph[r[1]-1].add(new int[]{r[0]-1, r[2]});
         }
         
-        int[] dist = new int[N];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
-        dist[0] = 0;
+        int[] costs = new int[N];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[0] = 0;
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-        pq.offer(new int[]{0, 0});
+        pq.offer(new int[]{0,0});
         
+        int answer = 1;
         while(!pq.isEmpty()){
-            int[] curr = pq.poll();
-            int cur = curr[0];
-            for(int[] next : graph[cur]){
-                if(dist[cur] + next[1] > dist[next[0]]) continue;
+            int[] current = pq.poll();
+            int node = current[0], cost = current[1];
+            
+            for(int[] next : graph[node]){
+                if(cost+next[1]>K || costs[next[0]]<cost+next[1]) continue;
                 
-                dist[next[0]] = dist[cur] + next[1]; 
-                pq.offer(new int[]{next[0], dist[next[0]]});
+                if(costs[next[0]]==Integer.MAX_VALUE) answer++;
+                costs[next[0]] = cost+next[1];
+                pq.offer(new int[]{next[0], cost+next[1]});
             }
         }
         
-        int answer = 0;
-        for(int d : dist) if(d<=K) answer++;
-        
+
         return answer;
     }
 }
