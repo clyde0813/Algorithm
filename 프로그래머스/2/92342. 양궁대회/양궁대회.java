1b59528d45803ec676a57731b9c51076e1d860c0
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-07 00:48:00 +0900

    [level 2] Title: 양궁대회, Time: 0.59 ms, Memory: 79.9 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,61 +1,66 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Solution {
-    private static int[] answer = new int[11];
-    private static int maxScoreDiff = 0;
-    private static int n;
-    private static int[] info;
+public class Solution {
 
-    public int[] solution(int n, int[] info) {
-        this.n = n;
-        this.info = info;
-        
-        dfs(0, n, new int[11]);
-        
-        if(maxScoreDiff==0) return new int[]{-1};
-        return answer;
+    private static int max;
+    private static int[] answer;
+    private static int[] apeach;
+
+    // ❶ 주어진 조합에서 각각의 점수 계산
+    private static int getScore(int[] ryan) {
+        int score = 0;
+        for (int i = 0; i <= 10; i++) {
+            if (ryan[i] + apeach[i] > 0) {
+                score += ryan[i] > apeach[i] ? (10 - i) : -(10 - i);
+            }
+        }
+        return score;
     }
-    
-    private static void dfs(int idx, int remain, int[] ryan) {
-        if(idx==11) {
-            int scoreDiff = getScoreDiff(ryan);
-            
-            if(scoreDiff<maxScoreDiff) return;
-            if(scoreDiff==maxScoreDiff){
-                for(int i=10; i>=0; i--){
-                    if(ryan[i]>answer[i]) break;
-                    if(ryan[i]<=answer[i] && remain>0) break;
-                    if(ryan[i]<answer[i]) return;
+
+    // ❷ 최대 차이와 라이언의 과녁 저장
+    private static void calculateDiff(int[] ryan) {
+        int score = getScore(ryan);
+        if (max < score) {
+            max = score;
+            answer = ryan.clone();
+        }
+        // 점수가 같으면 가장 낮은 점수를 더 많이 맞힌 경우를 찾음
+        else if (max > 0 && max == score) {
+            for (int i = 10; i >= 0; i--) {
+                if(answer[i] != ryan[i]) {
+                    if (answer[i] < ryan[i]) {
+                        answer = ryan.clone();
+                    }
+                    break;
                 }
             }
-            
-            answer = ryan.clone();
-            answer[10] += remain;
-            maxScoreDiff = scoreDiff;
+        }
+    }
 
+    // ❸ 가능한 라이언의 과녁 점수 조합의 모든 경우를 구함
+    private static void backtrack(int n, int idx, int[] ryan) {
+        if (n == 0) {
+            calculateDiff(ryan);
             return;
         }
-        
 
-        dfs(idx+1, remain, ryan);
-        
-        int need = info[idx]+1;
-        if(remain>=need) {
-            ryan[idx] = need;
-            dfs(idx+1, remain-need, ryan);
-            ryan[idx] = 0;
+        for (int i = idx; i <= 10; i++) {
+            int cnt = i == 10 ? n : apeach[i] + 1;
+            if (cnt > n) {
+                continue;
+            }
+            ryan[i] = cnt;
+            backtrack(n - cnt, i + 1, ryan);
+            ryan[i] = 0;
         }
-        return;
-}
-    
-    private static int getScoreDiff(int[] ryan) {
-        int value = 0;
-        for(int i=0; i<10; i++) {
-            if(info[i]+ryan[i]==0) continue;
-            
-            if(info[i]<ryan[i]) value += 10-i;
-            else value -= 10-i;
-        }
-        return value;
     }
+
+    public static int[] solution(int n, int[] info) {
+        apeach = info;
+        max = 0;
+        backtrack(n, 0, new int[11]);
+        // ❹ 최대 차이가 0인 경우 -1 반환, 아니면 answer 반환
+        return max == 0 ? new int[]{-1} : answer;
+    }
+
 }
