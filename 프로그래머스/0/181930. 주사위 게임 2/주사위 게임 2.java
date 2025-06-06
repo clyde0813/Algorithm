d5949dffb80a1e79d42c5d97f8add192dd9a548c
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-06 20:08:17 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,15 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int solution(int a, int b, int c) {
-        int status = compare(a, b, c);
-        if(status == 1) return (a+b+c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);
-        else if(status == 2) return (a+b+c) * (a*a + b*b + c*c);
-        else return (a+b+c);
-    }
-    
-    public static int compare(int a, int b, int c){
-        Set<Integer> set = new HashSet<>(Arrays.asList(a,b,c));
-        return set.size();
+        int answer = a+b+c;
+
+        if(a==b&&b==c) {
+            answer = answer * (a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }
+        else if(a==b||a==c||b==c) {
+            answer = answer * (a*a+b*b+c*c);
+        }
+        
+        return answer;
     }
 }
