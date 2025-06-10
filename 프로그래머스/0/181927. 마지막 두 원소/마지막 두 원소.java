9871bd75f71624415c90e03bc389c26c288f799b
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-10 10:34:01 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -6,9 +6,7 @@ class Solution {
         int[] answer = new int[n+1];
         
         for(int i=0; i<n; i++) answer[i] = num_list[i];
-        
-        if(num_list[n-1]>num_list[n-2]) answer[n] = num_list[n-1] - num_list[n-2];
-        else answer[n] = num_list[n-1] * 2;
+        answer[n] = (num_list[n-1]>num_list[n-2]) ? num_list[n-1] - num_list[n-2] : num_list[n-1] * 2;
         
         return answer;
     }
