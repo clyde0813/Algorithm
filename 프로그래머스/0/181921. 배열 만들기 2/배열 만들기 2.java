7928112be3c97c07a0f7001965a0de2005fdbb22
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-20 22:12:27 +0900

    [level 0] Title: 배열 만들기 2, Time: 2.77 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -3,18 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int l, int r) {
         List<Integer> result = new ArrayList<>();
-        
+
         for(int i=0; ;i++) {
             String binary = Integer.toBinaryString(i);
             StringBuilder sb = new StringBuilder();
             
-            for(char c : binary.toCharArray()) sb.append((c=='0') ? "0" : "5");    
-            
+            for(char c : binary.toCharArray()) {
+                sb.append((c=='0') ? "0" : "5");    
+            }
+
             int value = Integer.valueOf(sb.toString());
+
             if(value>r) break;
             else if(value>=l) result.add(value);
         }
-        
+
         return result.isEmpty() ? new int[]{-1} : result.stream().mapToInt(i->i).toArray();
     }
 }
