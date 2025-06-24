e3690f1d78b0f1ed22eab5135db7410716328d39
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-24 23:23:19 +0900

    [level 0] Title: 배열 만들기 4, Time: 21.83 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        int i = 0;
+        Deque<Integer> stk = new ArrayDeque<>();
+        
+        while(i<arr.length) {
+            if(stk.isEmpty()) stk.offerFirst(arr[i++]);
+            else {
+                if(stk.peekLast()<arr[i]) stk.offerLast(arr[i++]);
+                else stk.pollLast();
+            }
+        }
+                
+        return stk.stream().mapToInt(o->o).toArray();
+    }
+}
