b4929bfd56d853146433d67f7951c73cb02679d2
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-25 20:24:16 +0900

    [level 2] Title: 양궁대회, Time: 0.57 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,59 +1,59 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class Solution {
-    private static int max;
+class Solution {
+    private static int max = 0;
     private static int[] answer;
     private static int[] apeach;
-
+    
+    public int[] solution(int n, int[] info) {
+        this.apeach = info;
+        this.answer = new int[11];
+        
+        backtrack(n, 0, new int[11]);
+       	
+        return (max==0) ? new int[]{-1} : answer;
+    }
+    
     private static int getScore(int[] ryan) {
         int score = 0;
-        for (int i = 0; i <= 10; i++) {
-            if (ryan[i] + apeach[i] > 0) {
-                score += ryan[i] > apeach[i] ? (10 - i) : -(10 - i);
-            }
-        }
+        for(int i=0; i<11; i++) if(ryan[i]+apeach[i]!=0) score += (ryan[i]>apeach[i]) ? (10-i) : -(10-i);
         return score;
     }
-
-    private static void calculateDiff(int[] ryan) {
+    
+    private static void check(int[] ryan) {
         int score = getScore(ryan);
-        if (max < score) {
-            max = score;
-            answer = ryan.clone();
-        }
-        else if (max > 0 && max == score) {
-            for (int i = 10; i >= 0; i--) {
-                if(answer[i] != ryan[i]) {
-                    if (answer[i] < ryan[i]) {
-                        answer = ryan.clone();
-                    }
-                    break;
+        if(score<=0) return;
+        
+        if(score>max) updateAnswerAndMax(score, ryan);
+        else if(score==max) {
+            for(int i=10; i>=0; i--) {
+                if(ryan[i]!=answer[i]) {
+                    if(ryan[i]>answer[i]) updateAnswerAndMax(score, ryan);
+                    else return;
                 }
             }
         }
+        return;
     }
-
+    
+    private static void updateAnswerAndMax(int score, int[] ryan) {
+        answer = ryan.clone();
+        max = score;
+    }
+    
     private static void backtrack(int n, int idx, int[] ryan) {
-        if (n == 0) {
-            calculateDiff(ryan);
+        if(n==0) {
+            check(ryan);
             return;
         }
-
-        for (int i = idx; i <= 10; i++) {
-            int cnt = i == 10 ? n : apeach[i] + 1;
-            if (cnt > n) {
-                continue;
-            }
-            ryan[i] = cnt;
-            backtrack(n - cnt, i + 1, ryan);
+        
+        for(int i=idx; i<11; i++) {
+            int need = apeach[i] + 1;
+            if(i==10) need = n;
+            if(n<need) continue;
+            ryan[i] = need;
+            backtrack(n-need, i+1, ryan);
             ryan[i] = 0;
-        }
-    }
-
-    public static int[] solution(int n, int[] info) {
-        apeach = info;
-        max = 0;
-        backtrack(n, 0, new int[11]);
-        return max == 0 ? new int[]{-1} : answer;
-    }
+        }        
+    } 
 }
