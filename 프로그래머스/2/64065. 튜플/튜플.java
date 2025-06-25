19fcd48b2dfad78af439dcca29e0f74551bda45f
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-25 18:55:44 +0900

    [level 2] Title: 튜플, Time: 65.08 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -3,14 +3,14 @@ import java.util.*;
 class Solution {
     public int[] solution(String s) {
         String S = s.substring(0, s.length()-2).replace("{", "");
-        String[] sArray = S.split("},");
-        Arrays.sort(sArray, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
+        String[] arr = S.split("},");
+        Arrays.sort(arr, (o1, o2) -> Integer.compare(o1.length(), o2.length()));
         
         Set<String> set = new HashSet<>();
-        int[] answer = new int[sArray.length];
+        int[] answer = new int[arr.length];
         
-        for(int i=0; i<sArray.length; i++) {
-            String[] temp = sArray[i].split(",");
+        for(int i=0; i<arr.length; i++) {
+            String[] temp = arr[i].split(",");
             for(String t : temp) {
                 if(set.contains(t)) continue;
                 set.add(t);
