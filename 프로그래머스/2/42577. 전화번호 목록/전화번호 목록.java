d372c6153e97f661e2fe8907236f87888872343a
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-06-29 19:22:10 +0900

    [level 2] Title: 전화번호 목록, Time: 363.73 ms, Memory: 98.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,26 +2,12 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Arrays.sort(phone_book, (s1, s2) -> s1.length()-s2.length());
-        Set<String> numbers = new HashSet<>();
-        
-        numbers.add(phone_book[0]);
-       	int max = phone_book[0].length();
-        for(int i=1; i<phone_book.length; i++) {
-            String phone = phone_book[i];
-            
-            StringBuilder sb = new StringBuilder();
-            int cnt = 1;
-            for(char p : phone.toCharArray()) {
-                sb.append(p);
-                if(numbers.contains(sb.toString())) return false;
-				if(cnt++ == max) break;
+        Arrays.sort(phone_book);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
-            
-            numbers.add(phone);
-            max = phone.length();
         }
-        
-        return true;
-    }
+    	return true;
+	}
 }
