20e29467b07f0bc159dccbdd848e44b2db526304
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-02 20:12:54 +0900

    [level 2] Title: 카펫, Time: 0.06 ms, Memory: 86 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,9 +3,9 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int total = brown + yellow;
-        int rPc = (brown/2) + 2;
-        int row = (rPc + (int) Math.sqrt(rPc*rPc - 4 * total)) / 2;
-        int column = (rPc - (int) Math.sqrt(rPc*rPc - 4 * total)) / 2;
+        int rPc = brown/2 + 2;
+        int row = (int) (rPc + Math.sqrt(rPc*rPc - 4*total)) / 2;
+        int column = (int) (rPc - Math.sqrt(rPc*rPc - 4*total)) / 2; 
         return new int[]{row, column};
     }
 }
