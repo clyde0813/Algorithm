a3a25999d2f2d4d3803d1ba7a9b505dec3571a6c
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-02 20:54:35 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 92.8 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -1,35 +1,26 @@
 class Solution {
-    private static int n, m;
-    
     public int[] solution(String[] keyinput, int[] board) {
-        this.n = board[1]/2;
-        this.m = board[0]/2;
-        
-        int[] answer = new int[2];
-        
-        for(String key : keyinput) {
-            if(key.equals("up")) {
-                if(!isValid(answer[1]+1, answer[0])) continue;
-                answer[1]++;
+        int halfWidth  = board[0] / 2;
+        int halfHeight = board[1] / 2;
+
+        int x = 0, y = 0;
+
+        for (String key : keyinput) {
+            int nx = x, ny = y;
+            switch (key) {
+                case "up"   -> ny++;
+                case "down" -> ny--;
+                case "left" -> nx--;
+                case "right"-> nx++;
+                default      -> {}  
             }
-            else if(key.equals("down")) {
-                if(!isValid(answer[1]-1, answer[0])) continue;
-                answer[1]--;
-            }
-            else if(key.equals("left")) {
-                if(!isValid(answer[1], answer[0]-1)) continue;
-                answer[0]--;
-            }
-            else {
-                if(!isValid(answer[1], answer[0]+1)) continue;
-                answer[0]++;
+            
+            if (Math.abs(nx) <= halfWidth && Math.abs(ny) <= halfHeight) {
+                x = nx;
+                y = ny;
             }
         }
-        
-        return answer;
-    }
-    
-    private static boolean isValid(int y, int x) {
-        return n>=y && y>=-n && m>=x && x>=-m;
+
+        return new int[]{x, y};
     }
 }
