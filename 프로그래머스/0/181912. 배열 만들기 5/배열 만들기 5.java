679bb7b9be591e0adbc5497c57885f77fbfa51c1
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-02 18:33:02 +0900

    [level 0] Title: 배열 만들기 5, Time: 23.32 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.java
@@ -2,27 +2,13 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] intStrs, int k, int s, int l) {
-        // 1) 먼저 조건을 만족하는 개수 세기
-        int count = 0;
-        for (String str : intStrs) {
-            // 범위 검증 (필요 시)
-            if (s < 0 || s + l > str.length()) 
-                continue;
-            int val = Integer.parseInt(str.substring(s, s + l));
-            if (val > k) count++;
+        List<Integer> result = new ArrayList<>();
+        
+        for(String str : intStrs) {
+            int sub = Integer.valueOf(str.substring(s, s+l));
+            if(k < sub) result.add(sub);
         }
-
-        // 2) 결과 배열 생성 및 값 채우기
-        int[] answer = new int[count];
-        int idx = 0;
-        for (String str : intStrs) {
-            if (s < 0 || s + l > str.length()) 
-                continue;
-            int val = Integer.parseInt(str.substring(s, s + l));
-            if (val > k) {
-                answer[idx++] = val;
-            }
-        }
-        return answer;
+        
+        return result.stream().mapToInt(i->i).toArray();
     }
 }
