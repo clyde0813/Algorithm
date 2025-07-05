8dc30eb7023a1181610352627cbb712ade3dd2cc
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-05 22:10:44 +0900

    [level 2] Title: 피보나치 수, Time: 3.78 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,13 +1,10 @@
 class Solution {
     public int solution(int n) {
-        int prev = 0;
-        int current = 1;
-        while(n-1>0){
-            int tmp = current + prev;
-            prev = current;
-            current = tmp % 1234567;
-            n--;
-        }
-        return current;
+        int[] fibo = new int[n+1];
+        fibo[1] = 1;
+        
+        for(int i=2; i<n+1; i++) fibo[i] = (fibo[i-1] + fibo[i-2]) % 1234567;
+        
+        return fibo[n];
     }
 }
