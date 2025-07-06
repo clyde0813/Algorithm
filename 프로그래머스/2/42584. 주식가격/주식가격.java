fc19597783539d7a060dc35c7489dba7f3eedf92
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-06 15:27:50 +0900

    [level 2] Title: 주식가격, Time: 7.75 ms, Memory: 70.2 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -2,48 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-		return solutionWithArray(prices);
-    }
-    
-    public int[] solutionWithArray(int[] prices){
         int n = prices.length;
-        int[] stack = new int[n];
         int[] answer = new int[n];
-        
-        int idx = -1;
-        for(int i = 0; i<n; i++){
-            if(idx != -1 && prices[stack[idx]] > prices[i]) {
-                while(idx != -1 && prices[stack[idx]] > prices[i]){
-                    answer[stack[idx]] = i - stack[idx--];
-                }
+        int[] stack = new int[n];
+    
+        int idx =-1;
+        for(int i=0; i<n; i++) {
+            if(idx!=-1 && prices[i]<prices[stack[idx]]) {
+                while(idx!=-1 && prices[i]<prices[stack[idx]]) 
+                    answer[stack[idx]] = i-stack[idx--];
             }
             stack[++idx] = i;
         }
         
-        for(int i = 0; i<n-1; i++){
-            if(answer[i] == 0) answer[i] = n - 1 - i;
-        }
-        
-        return answer;
-    }
-    
-    public int[] solutionWithDeque(int[] prices){
-        int n = prices.length;
-        Deque<Integer> deque = new ArrayDeque<>();
-        int[] answer = new int[n];
-        
-        for(int i = 0; i<n; i++){
-            if(!deque.isEmpty() && prices[deque.peek()] > prices[i]){
-                while(!deque.isEmpty() && prices[deque.peek()] > prices[i]){
-                    int last = deque.pop();
-                    answer[last] = i - last;
-                }
-            } 
-            deque.push(i);
-        }
-        
-        for(int i = 0; i<n-1; i++){
-            if(answer[i] == 0) answer[i] = n - 1 - i;
+        for(int i=0; i<n; i++) {
+            if(answer[i]==0) answer[i] = n - i - 1;
         }
         
         return answer;
