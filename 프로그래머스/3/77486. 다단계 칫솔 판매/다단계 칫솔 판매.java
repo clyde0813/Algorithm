6995dd02e133a3345dc5532259c2f2d4511e4250
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-06 17:14:53 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 59.74 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,37 +2,24 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int enrollCount = enroll.length;
-        int[] answer = new int[enrollCount];
-        Map<String, Node> dict = new HashMap<>();
-        for(int i=0; i<enrollCount; i++){
-            Node parentNode = referral[i].equals("-") ? null : dict.get(referral[i]);
-            Node node = new Node(enroll[i], parentNode);
-            dict.put(enroll[i], node);
+        int[] answer = new int[enroll.length];
+        Map<String, Integer> idxMap = new HashMap<>();
+        Map<String, String> treeMap = new HashMap<>();
+        for(int i=0; i<enroll.length; i++) {
+            idxMap.put(enroll[i], i);
+            if(!referral[i].equals("-")) treeMap.put(enroll[i], referral[i]);
         }
-        for(int i=0; i<seller.length; i++){
-            Node sellerNode = dict.get(seller[i]);
-            int sellerProfit = amount[i] * 100;
-            while(sellerNode != null){
-                sellerNode.profit += sellerProfit - (int) (sellerProfit * 0.1);
-                sellerProfit = (int) (sellerProfit * 0.1);
-                sellerNode = sellerNode.parent;
+        
+        for(int i=0; i<seller.length; i++) {
+            int profit = amount[i]*100;
+            String target = seller[i];
+            while(target!=null && profit!=0 ) {
+                answer[idxMap.get(target)] += profit - (int) (profit * 0.1);
+                target = treeMap.get(target);
+                profit = (int) (profit * 0.1);
             }
         }
-        for(int i=0; i<enrollCount; i++){
-            answer[i] = dict.get(enroll[i]).profit;
-        }
+        
         return answer;
-    }
-    
-    static class Node {
-        private String name;
-        private Node parent;
-        private int profit;
-        public Node(String name, Node parent){
-            this.name = name;
-            this.parent = parent;
-            this.profit = 0;
-        }
     }
 }
