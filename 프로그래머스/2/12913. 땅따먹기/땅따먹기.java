b9cbb542f2482ce86382b67d32c93e54eca6a5e4
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-06 16:39:35 +0900

    [level 2] Title: 땅따먹기, Time: 39.17 ms, Memory: 106 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -5,9 +5,7 @@ class Solution {
         for(int i=1; i<land.length; i++) {
             for(int j=0; j<4; j++) {
                 int max = 0;
-                for(int p=0; p<4; p++) {
-                    if(p!=j) max = Math.max(max, land[i-1][p]);
-                }
+                for(int p=0; p<4; p++) if(p!=j) max = Math.max(max, land[i-1][p]);
                 land[i][j] += max;
             }
         }
