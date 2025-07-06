0f7a52acbeb8a3f3b0cb09990e5918d5a06141a6
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-06 17:23:42 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 27.86 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int[] answer = new int[enroll.length];
+        int n = enroll.length;
+        int[] answer = new int[n];
         Map<String, Integer> idxMap = new HashMap<>();
-        Map<String, String> treeMap = new HashMap<>();
-        for(int i=0; i<enroll.length; i++) {
-            idxMap.put(enroll[i], i);
-            if(!referral[i].equals("-")) treeMap.put(enroll[i], referral[i]);
-        }
+        for(int i=0; i<n; i++) idxMap.put(enroll[i], i);
+        
+        int[] parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = referral[i].equals("-") ? -1 : idxMap.get(referral[i]);
         
         for(int i=0; i<seller.length; i++) {
             int profit = amount[i]*100;
-            String target = seller[i];
-            while(target!=null && profit!=0 ) {
-                answer[idxMap.get(target)] += profit - (int) (profit * 0.1);
-                target = treeMap.get(target);
-                profit = (int) (profit * 0.1);
+            int cur = idxMap.get(seller[i]);
+            while(cur!=-1 && profit>0) {
+                int commission = profit / 10;
+                answer[cur] += profit - commission;
+                cur = parent[cur];
+                profit = commission;
             }
         }
         
