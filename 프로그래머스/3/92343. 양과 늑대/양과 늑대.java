38272e9e2f7e4031bbf22983d898c63f43fe0275
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-07 19:08:31 +0900

    [level 3] Title: 양과 늑대, Time: 3.55 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -1,45 +1,45 @@
 import java.util.*;
 
-public class Solution {
-    private List<Integer>[] adj;    
-    private int answer = 0;
-    private int[] info;           
-
+class Solution {
+    private int[] info;
+    private List<Integer>[] tree;
+    private int answer;
+    
     public int solution(int[] info, int[][] edges) {
-        this.info = info;
         int n = info.length;
-        adj = new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<>();
-        }
-        for (int[] e : edges) {
-            adj[e[0]].add(e[1]);
-        }
-
-        List<Integer> next = new ArrayList<>();
-        next.add(0);
-        dfs(next, 0, 0);
+        this.info = info;    
+        this.tree = new ArrayList[n];
+        
+        for(int i=0; i<n; i++) tree[i] = new ArrayList<>();
+        
+        for(int[] edge : edges) tree[edge[0]].add(edge[1]);
+        
+        List<Integer> visitable = new ArrayList<>();
+        visitable.add(0);
+        
+        dfs(0, 0, visitable);
+        
         return answer;
     }
-
-    private void dfs(List<Integer> nextList, int sheep, int wolf) {
+    
+    private void dfs(int sheep, int wolf, List<Integer> visitable) {
         answer = Math.max(answer, sheep);
-
-        for (int i = 0; i < nextList.size(); i++) {
-            int node = nextList.get(i);
-            int ns = sheep + (info[node] == 0 ? 1 : 0);
-            int nw = wolf  + (info[node] == 1 ? 1 : 0);
-
-            if (nw >= ns) continue;
-
-            List<Integer> nxt = new ArrayList<>(nextList);
-            nxt.remove(i);         
-
-            for (int child : adj[node]) {
-                nxt.add(child);
-            }
-
-            dfs(nxt, ns, nw);
+        
+        for(int i=0; i<visitable.size(); i++) {
+            int edge = visitable.get(i);
+            int ns = sheep + (info[edge] == 0 ? 1 : 0);
+            int nw = wolf + (info[edge] == 1 ? 1 : 0);
+            
+            if(ns<=nw) continue;
+            
+            List<Integer> newVisitable = new ArrayList<>(visitable);
+            newVisitable.remove(i);
+            
+            for(int child : tree[edge]) newVisitable.add(child);
+            
+            dfs(ns, nw, newVisitable);
         }
-    }
+        
+        return;
+    } 
 }
