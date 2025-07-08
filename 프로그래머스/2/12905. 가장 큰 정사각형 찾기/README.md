5a1b5966ff4242dabfd0e1cb663e5fce4288a2a5
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-08 20:46:01 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 17.25 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 정사각형 찾기 - 12905 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12905#) 
 
 ### 성능 요약
 
-메모리: 90 MB, 시간: 38.52 ms
+메모리: 90.5 MB, 시간: 17.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 20:16:24
+2025년 07월 08일 20:45:58
 
 ### 문제 설명
 
--- 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.java
@@ -1,42 +1,23 @@
-import java.util.*;
-
-class Solution {
-    private int[][] board;
-    private int n, m;
-    private int[][] dirs = {{-1,-1}, {-1,0}, {0,-1}};
-    
-    public int solution(int[][] board) {
-        this.board = board;
-        this.n = board.length;
-        this.m = board[0].length;
+class Solution
+{
+    public int solution(int [][]board) {
+        int n = board.length;
+        int m = board[0].length;
+        int answer = 0;
         
-        int side = 0;
-        
-        for(int y=0; y<n; y++) {
-            for(int x=0; x<m; x++) {
-                if(board[y][x]==0) continue;
-                if(isInSquare(y, x)) board[y][x] = findMinValue(y, x) + 1;
-                side = Math.max(side, board[y][x]);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(i!=0 && j!=0 && board[i][j]==1) {
+                    int up = board[i-1][j];
+                    int left = board[i][j-1];
+                    int upleft = board[i-1][j-1];
+                    
+                    board[i][j] += Math.min(upleft, Math.min(up, left));
+                }
+                answer = Math.max(answer, board[i][j]);
             }
-        } 
-        
-        return side * side;
-    }
-    
-    private boolean isInSquare(int y, int x) {
-        if(y==0||x==0) return false;
-        
-        for(int[] dir : dirs) {
-            if(board[y + dir[0]][x + dir[1]]==0) return false;
         }
-        return true;
-    }
-    
-    private int findMinValue(int y, int x) {
-        int value = Integer.MAX_VALUE;
-        for(int[] dir : dirs) {
-            value = Math.min(value, board[y + dir[0]][x + dir[1]]);
-        }
-        return value;
+
+        return answer * answer;
     }
 }
