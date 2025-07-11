98b93d30583f4f976f907c614d55cacd9309e7ad
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-11 19:59:35 +0900

    [level 3] Title: 기지국 설치, Time: 0.99 ms, Memory: 55 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int n, int[] stations, int w) {
         int answer = 0;
-        int location = 0;
+        int location = 1;
         int idx = 0;
         
-        while(location<n) {
-            if(idx<stations.length && stations[idx]-w-1<=location) {
-               location = stations[idx++] + w;
+        while(location<=n) {
+            if(idx<stations.length && stations[idx]-w<=location) {
+               location = stations[idx++] + w + 1;
             } 
             else {
                 location += 2*w+1;
