c547d380fbc3a98d62d937518619ff5e144cfddb
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-12 14:58:26 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 19.77 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -2,25 +2,46 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        int n = enroll.length;
-        int[] answer = new int[n];
-        Map<String, Integer> idxMap = new HashMap<>();
-        for(int i=0; i<n; i++) idxMap.put(enroll[i], i);
+        Map<String, Node> tree = buildTree(enroll, referral);
+        calculateAmount(tree, seller, amount);
         
-        int[] parent = new int[n];
-        for(int i=0; i<n; i++) parent[i] = referral[i].equals("-") ? -1 : idxMap.get(referral[i]);
-        
-        for(int i=0; i<seller.length; i++) {
-            int profit = amount[i]*100;
-            int cur = idxMap.get(seller[i]);
-            while(cur!=-1 && profit>0) {
-                int commission = profit / 10;
-                answer[cur] += profit - commission;
-                cur = parent[cur];
-                profit = commission;
-            }
-        }
+        int[] answer = new int[enroll.length];
+        for(int i=0; i<enroll.length; i++) answer[i] = tree.get(enroll[i]).amount;
         
         return answer;
+    }
+    
+    private Map<String, Node> buildTree(String[] enroll, String[] referral) {
+        Map<String, Node> result = new HashMap<>();
+        
+        for(int i=0; i<enroll.length; i++) {
+            if(referral[i].equals("-")) result.put(enroll[i], new Node(0, null));
+            else result.put(enroll[i], new Node(0, result.get(referral[i])));
+        }
+                        
+        return result;
+    }
+    
+    private void calculateAmount(Map<String, Node> tree, String[] seller, int[] amount) {
+        for(int i=0; i<seller.length; i++) {
+            Node current = tree.get(seller[i]);
+            int curAmount = amount[i] * 100;
+            while(current!=null && curAmount!=0) {
+                int commission = curAmount / 10;
+                current.amount += curAmount - commission;
+                curAmount = commission;
+                current = current.parent;
+            }
+        }
+    }
+    
+    private static class Node {
+        int amount;
+        Node parent;
+        
+        public Node(int amount, Node parent) {
+            this.amount = amount;
+            this.parent = parent;
+        }
     }
 }
