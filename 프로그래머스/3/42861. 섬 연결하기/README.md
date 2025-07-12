aed9ced130da695f0ccbf467044b9ff807177546
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-12 22:58:27 +0900

    [level 3] Title: 섬 연결하기, Time: 0.92 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 0.73 ms
+메모리: 77.8 MB, 시간: 0.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 12일 22:53:28
+2025년 07월 12일 22:58:25
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 class Solution {
-    private int[] root;
+    private int[] parent;
     
     public int solution(int n, int[][] costs) {
         if(n==1) return 0;
         
-        this.root = new int[n];
-        for(int i=0; i<n; i++) root[i] = i;
+        this.parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
         
         Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
         
@@ -15,9 +15,7 @@ class Solution {
         for(int[] cost : costs) {
             if(find(cost[0]) == find(cost[1])) continue;
             
-            int parent = Math.min(cost[0], cost[1]);
-            int child = Math.max(cost[0], cost[1]);
-            union(parent, child);
+            union(cost[0], cost[1]);
             answer += cost[2];
         }
         
@@ -25,13 +23,11 @@ class Solution {
     }
     
     private int find(int node) {
-        if(root[node] != node) root[node] = find(root[node]);
-        return root[node];
+        if(parent[node] != node) parent[node] = find(parent[node]);
+        return parent[node];
     }
     
-    private void union(int parent, int child) {
-        int parentRoot = find(parent);
-        int childRoot = find(child);
-        root[childRoot] = parentRoot;        
+    private void union(int node1, int node2) {
+        parent[find(node1)] = find(node2);        
     }
 }
