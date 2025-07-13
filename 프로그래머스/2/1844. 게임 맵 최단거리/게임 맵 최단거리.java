f7ab6b2b699b3e65d2afebab04a4065750a580db
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-13 20:05:25 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.55 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,31 +1,43 @@
 import java.util.*;
 
 class Solution {
+    private int n, m;
+    private int[][] maps;
+    private boolean[][] visited;
+    private int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+    
     public int solution(int[][] maps) {
-        int n = maps[0].length;
-        int m = maps.length;
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        boolean[][] visited = new boolean[m][n];
+        this.n = maps.length;
+        this.m = maps[0].length;
+        this.maps = maps;
+        this.visited = new boolean[n][m];
         
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0,0,1});
+        Deque<int[]> deque = new ArrayDeque<>();
+        deque.offerFirst(new int[]{0,0,1});
         visited[0][0] = true;
         
-        while(!queue.isEmpty()){
-            int[] curr = queue.poll();
-            int y = curr[0], x = curr[1], v = curr[2];
-            
-            if(y==m-1&&x==n-1) return v;
-            
-            for(int[] dir : dirs){
-                int ny = y+dir[0], nx = x+dir[1];
-                if(m<=ny||ny<0||n<=nx||nx<0) continue;
-                if(visited[ny][nx]||maps[ny][nx]==0) continue;
-                visited[ny][nx] = true;
-                queue.offer(new int[]{ny, nx, v+1});   
+        while(!deque.isEmpty()) {
+            int[] curr = deque.pollLast();
+            int y = curr[0], x = curr[1], count = curr[2];
+            if(y==n-1&&x==m-1) return count;
+
+            for(int[] dir : dirs) {
+                int dy = y+dir[0], dx = x+dir[1];
+                if(!isInBound(dy, dx)) continue;
+                if(!isAvailable(dy, dx)) continue;
+                visited[dy][dx] = true;
+                deque.offerFirst(new int[]{dy, dx, count+1});
             }
         }
         
         return -1;
+    }
+    
+    private boolean isInBound(int y, int x) {
+        return n>y && y>=0 && m>x && x>=0;
+    }
+    
+    private boolean isAvailable(int y, int x) {
+        return maps[y][x]!=0 && !visited[y][x];
     }
 }
