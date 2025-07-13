f3dc8f3e921d6e165e34f9c33ce87d259a998f14
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-13 22:25:44 +0900

    [level 3] Title: 네트워크, Time: 3.49 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,28 +1,35 @@
 import java.util.*;
+import java.util.stream.*;
 
 class Solution {
+    private int[] parent;
+    
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
+        this.parent = new int[n];
+        for(int i=0; i<n; i++) parent[i] = i;
         
         int answer = 0;
-        for(int i=0; i<n; i++){
-            if(visited[i]==false){
-                dfs(computers, visited, i, n);
-                answer++;
+        for(int i=0; i<n; i++) {
+            int[] connections = computers[i];
+            for(int j=0; j<n; j++) {
+                if(i==j || connections[j]==0) continue;
+                
+                if(find(i) != find(j)) union(i, j);
             }
         }
+        for(int i=0; i<n; i++) find(i);
         
-        return answer;
+        Set<Integer> set = new HashSet<>(Arrays.stream(parent).boxed().collect(Collectors.toList()));
+
+        return set.size();
     }
     
-    private void dfs(int[][] computers, boolean[] visited, int target, int n){
-        if(visited[target]==true) return;
-        
-        visited[target] = true;
-        for(int i=0; i<n; i++){
-            if(computers[target][i]==1){
-                dfs(computers, visited, i, n);
-            }
-        }
+    private int find(int node) {
+        if(parent[node] != node) parent[node] = parent[find(parent[node])];
+        return parent[node];
+    }
+    
+    private void union(int node1, int node2) {
+        parent[find(node1)] = find(node2); 
     }
 }
