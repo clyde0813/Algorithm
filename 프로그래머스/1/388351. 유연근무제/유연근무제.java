ad51fc8f499f215279d9550a79d15bff53473ba6
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-13 14:23:16 +0900

    [level 1] Title: 유연근무제, Time: 0.49 ms, Memory: 79.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/388351. 유연근무제/유연근무제.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int solution(int[] schedules, int[][] timelogs, int startday) {
+        int answer = 0;
+        
+        for(int i=0; i<schedules.length; i++) {
+            int pivot = addMinutes(schedules[i], 10);
+            int day = startday;
+            boolean flag = true;
+            
+            for(int timelog : timelogs[i]) {
+                if(day == 6){
+                    day++;
+                    continue;
+                } 
+                else if(day == 7) {
+                    day = 1; 
+                    continue;
+                }
+                
+                if(timelog > pivot){
+                    flag = false; 
+                    break;
+                } 
+                day++;
+            }
+            if(flag) answer++;
+        }
+        
+        return answer;
+    }
+    
+    private int addMinutes(int base, int minutes) {
+        base += minutes;
+        if(base % 100 >= 60) {
+            base += 100;
+            base -= 60;
+        }
+        
+        return base;
+    }
+}
