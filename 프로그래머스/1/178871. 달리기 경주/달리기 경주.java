39e2cabccd1479d28d626b457baa93a1b08f5270
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-15 19:11:40 +0900

    [level 1] Title: 달리기 경주, Time: 274.98 ms, Memory: 369 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.java
@@ -3,26 +3,21 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
         int n = players.length;
-        Map<String, Integer> nameRank = new HashMap<>();
-        Map<Integer, String> rankName = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
         
-        for(int i=0; i<n; i++) {
-            nameRank.put(players[i], i);
-            rankName.put(i, players[i]);
-        }
+        for(int i=0; i<n; i++) idxMap.put(players[i], i);
         
         for(String call : callings) {
-            int rank = nameRank.get(call);
-            String rival = rankName.get(rank-1);
-            nameRank.put(call, rank-1);
-            rankName.put(rank-1, call);
-            nameRank.put(rival, rank);
-            rankName.put(rank, rival);
+            int cur = idxMap.get(call);
+            
+            String temp = players[cur];
+            players[cur] = players[cur-1];
+            players[cur-1] = temp;
+            
+            idxMap.put(players[cur], cur);
+            idxMap.put(players[cur-1], cur-1);
         }
         
-        String[] answer = new String[n];
-        for(int i=0; i<n; i++) answer[i] = rankName.get(i);
-        
-        return answer;
+        return players;
     }
 }
