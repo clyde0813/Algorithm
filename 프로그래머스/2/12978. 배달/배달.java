bef659e8be338db8b3b791328ad12f3347421dff
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-16 20:19:48 +0900

    [level 2] Title: 배달, Time: 13.04 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 class Solution {
     public int solution(int N, int[][] road, int K) {
-        Map<Integer, List<int[]>> map = new HashMap<>();
-        for(int[] r : road) {
-            map.computeIfAbsent(r[0], o -> new ArrayList<>()).add(new int[]{r[0],r[1],r[2]});
-            map.computeIfAbsent(r[1], o -> new ArrayList<>()).add(new int[]{r[1],r[0],r[2]});
-        }
+        if(N==1) return 1;
+        
+        List<int[]>[] graph = new ArrayList[N];
+        for(int i=0; i<N; i++) graph[i] = new ArrayList<>();
+        for(int[] r : road) for(int i=0; i<2; i++) graph[r[i]-1].add(new int[]{r[i]-1, r[1-i]-1, r[2]});
         
         PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[2] - o2[2]);
-        pq.addAll(map.get(1));
+        pq.addAll(graph[0]);
         
         int[] cost = new int[N];
         Arrays.fill(cost, Integer.MAX_VALUE);
@@ -18,13 +18,13 @@ class Solution {
         int answer = 1;
         while(!pq.isEmpty()) {
             int[] current = pq.poll();
-            int parent = current[0]-1, child = current[1]-1, value = current[2];
+            int parent = current[0], child = current[1], value = current[2];
             int currentCost = cost[parent] + value;
             
             if(currentCost<=K && currentCost<cost[child]) {
-                if(map.containsKey(child+1)) pq.addAll(map.get(child+1));
                 if(cost[child]==Integer.MAX_VALUE) answer++;
                 cost[child] = currentCost;
+                pq.addAll(graph[child]);
             }
         }
 
