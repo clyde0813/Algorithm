8fab94511d85d0c9b206d22c83117f7b86aeadde
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-17 23:36:08 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.16 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = new int[4];
+        answer[0] = Integer.MAX_VALUE; answer[1] = Integer.MAX_VALUE;
+        answer[2] = 0; answer[3] = 0;
+        
+        for(int y=0; y<wallpaper.length; y++) {
+            for(int x=0; x<wallpaper[0].length(); x++) {
+                if(wallpaper[y].charAt(x)!='#') continue;
+                
+                if(y<answer[0]) answer[0]=y;
+                if(x<answer[1]) answer[1]=x;
+                
+                if(y>answer[2]) answer[2]=y;
+                if(x>answer[3]) answer[3]=x;
+            }
+        }
+        answer[2]++; answer[3]++;
+        return answer;
+    }
+}
