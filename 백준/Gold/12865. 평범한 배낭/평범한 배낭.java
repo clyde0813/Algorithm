530aa6147c1f9007041bd15c59e7d6478b646698
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-19 17:46:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 184 ms, Memory: 54160 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -20,23 +18,23 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[n+1][k+1];
-        int[][] arr = new int[n+1][2];
+        int[][] dp = new int[n + 1][k + 1];
+        int[][] arr = new int[n + 1][2];
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            arr[i] = new int[]{Integer.valueOf(st.nextToken()), Integer.valueOf(st.nextToken())};
+            arr[i] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
         }
         Arrays.sort(arr, (o1, o2) -> o1[0] - o2[0]);
 
-        for(int i=1; i<n+1; i++) {
+        for (int i = 1; i < n + 1; i++) {
             int[] current = arr[i];
             int w = current[0];
             int v = current[1];
 
-            for(int j=1; j<k+1; j++) {
-                if(j<w) dp[i][j] = dp[i-1][j];
-                else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-w]+v);
+            for (int j = 1; j < k + 1; j++) {
+                if (j < w) dp[i][j] = dp[i - 1][j];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
             }
         }
 
