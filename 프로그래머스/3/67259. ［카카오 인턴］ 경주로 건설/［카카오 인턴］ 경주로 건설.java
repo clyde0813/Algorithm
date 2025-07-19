48f9cb1be5dfab0bcfaaf30a55af7f2ed1cf3168
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-19 19:10:49 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 6.38 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -3,11 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int[][] board) {
         int n = board.length;
-        int m = board[0].length;
         int[][] dirs = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
-        int[][][] map = new int[n][m][4];
-        for(int i=0; i<n; i++) for(int j=0; j<m; j++) Arrays.fill(map[i][j], Integer.MAX_VALUE);
+        int[][][] map = new int[n][n][4];
+        for(int i=0; i<n; i++) for(int j=0; j<n; j++) Arrays.fill(map[i][j], Integer.MAX_VALUE);
 
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
         pq.offer(new int[]{0, 0, 0, -1});
@@ -20,7 +19,7 @@ class Solution {
                 int[] dir = dirs[i];
                 int dy = y+dir[0], dx = x+dir[1];
 
-                if(!(n>dy&&dy>=0&&m>dx&&dx>=0) || board[dy][dx]==1) continue;
+                if(!(n>dy&&dy>=0&&n>dx&&dx>=0) || board[dy][dx]==1) continue;
 
                 int value = cost + ((direction == i || direction==-1) ? 100 : 600);
                 if(value >= map[dy][dx][i]) continue;
@@ -29,6 +28,6 @@ class Solution {
             }
         }
         
-        return Arrays.stream(map[n-1][m-1]).min().getAsInt();
+        return Arrays.stream(map[n-1][n-1]).min().getAsInt();
     }
 }
