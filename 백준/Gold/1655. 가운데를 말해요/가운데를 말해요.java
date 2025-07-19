3a98335c6f1284f84f83825c9a047d79ac468e07
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-19 20:57:16 +0900

    [Gold II] Title: 가운데를 말해요, Time: 392 ms, Memory: 29988 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -12,43 +12,22 @@ public class Main {
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
 
-        PriorityQueue<Integer> leftHeap = new PriorityQueue<>((o1, o2) -> o2 - o1);
-        PriorityQueue<Integer> rightHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> right = new PriorityQueue<>();
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            int value = Integer.parseInt(st.nextToken());
+            int value = Integer.parseInt(br.readLine());
 
-            if (i == 0) leftHeap.offer(value);
+            if(left.isEmpty() || value <= left.peek()) left.offer(value);
+            else right.offer(value);
 
-            else if (leftHeap.size() == rightHeap.size()) {
-                int temp = rightHeap.poll();
-                rightHeap.offer(temp);
-                if (temp >= value) leftHeap.offer(value);
-                else {
-                    leftHeap.offer(rightHeap.poll());
-                    rightHeap.offer(value);
-                }
-            }
+            if (left.size() - right.size() > 1) right.offer(left.poll());
+            else if (right.size() > left.size()) left.offer(right.poll());
 
-            else {
-                int temp = leftHeap.poll();
-                leftHeap.offer(temp);
-                if (temp <= value) {
-                    rightHeap.offer(value);
-                } else {
-                    rightHeap.offer(leftHeap.poll());
-                    leftHeap.offer(value);
-                }
-            }
-
-            value = leftHeap.poll();
-            leftHeap.offer(value);
-            bw.write(String.valueOf(value));
+            bw.write(String.valueOf(left.peek()));
             bw.newLine();
         }
 
