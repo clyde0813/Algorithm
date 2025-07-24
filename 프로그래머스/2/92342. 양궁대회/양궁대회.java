1f9ddcbf60c8f2a2a04eaac55948f686407904b7
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-24 20:57:16 +0900

    [level 2] Title: 양궁대회, Time: 0.52 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.java
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.java
@@ -1,59 +1,57 @@
 import java.util.*;
 
 class Solution {
-    private static int max = 0;
-    private static int[] answer;
-    private static int[] apeach;
+    private int score;
+    private int[] apeach;
+    private int[] answer = {-1};
     
     public int[] solution(int n, int[] info) {
         this.apeach = info;
-        this.answer = new int[11];
         
-        backtrack(n, 0, new int[11]);
-       	
-        return (max==0) ? new int[]{-1} : answer;
-    }
-    
-    private static int getScore(int[] ryan) {
-        int score = 0;
-        for(int i=0; i<11; i++) if(ryan[i]+apeach[i]!=0) score += (ryan[i]>apeach[i]) ? (10-i) : -(10-i);
-        return score;
-    }
-    
-    private static void check(int[] ryan) {
-        int score = getScore(ryan);
-        if(score<=0) return;
+        dfs(n, 0, new int[11]);
         
-        if(score>max) updateAnswerAndMax(score, ryan);
-        else if(score==max) {
-            for(int i=10; i>=0; i--) {
-                if(ryan[i]!=answer[i]) {
-                    if(ryan[i]>answer[i]) updateAnswerAndMax(score, ryan);
-                    else return;
-                }
-            }
-        }
-        return;
+        return answer;
     }
     
-    private static void updateAnswerAndMax(int score, int[] ryan) {
-        answer = ryan.clone();
-        max = score;
-    }
-    
-    private static void backtrack(int n, int idx, int[] ryan) {
+    private void dfs(int n, int idx, int[] ryan) {
         if(n==0) {
-            check(ryan);
+            getScore(ryan);
             return;
         }
         
         for(int i=idx; i<11; i++) {
-            int need = apeach[i] + 1;
-            if(i==10) need = n;
-            if(n<need) continue;
-            ryan[i] = need;
-            backtrack(n-need, i+1, ryan);
+            int require = (i==10) ? n : apeach[i]+1;
+            if(n < require) continue;
+            
+            ryan[i] = require;
+            dfs(n-require, i+1, ryan);
             ryan[i] = 0;
-        }        
-    } 
+        }
+        return;
+    }
+    
+    private void getScore(int[] ryan) {
+        int result = 0;
+        for(int i=0; i<11; i++) {
+            if(apeach[i] + ryan[i] == 0) continue;
+            result += (apeach[i] < ryan[i]) ? (10-i) : -(10-i);
+        }
+                
+        if(result==score && score!=0) {
+            for(int i=10; i>=0; i--) {
+                if(ryan[i] == answer[i]) continue;
+                
+                if(ryan[i] > answer[i]) {
+                    answer = ryan.clone();
+                    score = result;
+                    break;
+                } else break;
+            }   
+        }
+        else if(result > score){
+            answer = ryan.clone();
+            score = result;
+        } 
+        return;
+    }
 }
