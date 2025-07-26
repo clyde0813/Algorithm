95a836aa4f2844ed98237e02ecc93c97478855f3
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-26 18:10:42 +0900

    [level 3] Title: 사라지는 발판, Time: 23.17 ms, Memory: 86.1 MB -BaekjoonHub

--- 프로그래머스/3/92345. 사라지는 발판/README.md
+++ 프로그래머스/3/92345. 사라지는 발판/README.md
@@ -1,10 +1,10 @@
 # [level 3] 사라지는 발판 - 92345 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92345#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92345) 
 
 ### 성능 요약
 
-메모리: 93.2 MB, 시간: 11.88 ms
+메모리: 86.1 MB, 시간: 23.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 22일 15:36:51
+2025년 07월 26일 18:10:39
 
 ### 문제 설명
 
--- 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
+++ 프로그래머스/3/92345. 사라지는 발판/사라지는 발판.java
@@ -1,40 +1,40 @@
 import java.util.*;
 
 class Solution {
-    private static int[][] board;
-    private static int m, n;
-    private static int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
+    private int n, m;
+    private int[][] BOARD;
+    private int[][] dirs = {{-1,0},{1,0},{0,-1},{0,1}};
     
     public int solution(int[][] board, int[] aloc, int[] bloc) {
-        this.board = board;
+        this.BOARD = board;
         this.n = board.length;
         this.m = board[0].length;
-        
+                
         return dfs(aloc, bloc);
     }
     
-    private static int dfs(int[] current, int[] next) {
+    private int dfs(int[] current, int[] next) {
         int y = current[0], x = current[1];
-        if(!isValid(y, x)) return 0;
-        
-        int ret = 0;
-        board[y][x] = 0;
+        if(!isAvailable(y, x)) return 0;
+            
+        int result = 0;
+        BOARD[y][x] = 0;
         for(int[] dir : dirs) {
             int dy = y+dir[0], dx = x+dir[1];
-            if(!isValid(dy, dx)) continue;
+            if(!isAvailable(dy, dx)) continue;
             
-            int cnt = 1 + dfs(next, new int[]{dy, dx});
+            int count = dfs(next, new int[]{dy, dx})+1;
             
-            if(ret%2==0 && cnt%2==1)  ret = cnt;
-            else if(ret%2==0 && cnt%2==0) ret = Math.max(ret, cnt);
-            else if(ret%2==1 && cnt%2==1) ret = Math.min(ret, cnt);
+            if(result%2==0 && count%2==1) result = count;
+            else if(result%2==0 && count%2==0) result=Math.max(result, count);
+            else if(result%2==1 && count%2==1) result=Math.min(result, count);
         }
-		board[y][x] = 1;
+        BOARD[y][x] = 1;
         
-        return ret;
+        return result;
     }
     
-    private static boolean isValid(int y, int x) {
-        return m>x && x>=0 && n>y && y>=0 && board[y][x] == 1;
+    private boolean isAvailable(int y, int x) {
+        return n>y && y>=0 && m>x && x>=0 && BOARD[y][x]==1;
     }
 }
