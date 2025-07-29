6f49d759e8cc9926aaa85b9e9c384a8c69aefaa2
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-29 19:45:12 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 12.15 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,24 +1,25 @@
 import java.util.*;
-import java.util.stream.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        int todayInt = dateToInt(today);
+        int todayInt = dayToInt(today);
         Map<String, Integer> termMap = new HashMap<>();
+        for(String term : terms) {
+            termMap.put(term.split(" ")[0], Integer.valueOf(term.split(" ")[1])*28);
+        }
+        
         List<Integer> answer = new ArrayList<>();
-        
-        for(String term : terms) termMap.put(term.split(" ")[0], Integer.valueOf(term.split(" ")[1])*28);
-        
         for(int i=0; i<privacies.length; i++) {
-            String[] privacy = privacies[i].split(" ");
-            if(dateToInt(privacy[0]) + termMap.get(privacy[1]) <= todayInt) answer.add(i+1);            
+            String privacy = privacies[i];
+            int day = dayToInt(privacy.split(" ")[0]) + termMap.get(privacy.split(" ")[1]);
+            if(day<=todayInt) answer.add(i+1);
         }
         
         return answer.stream().mapToInt(i->i).toArray();
     }
     
-    private int dateToInt(String date) {
-        int[] arr = Arrays.stream(date.split("\\.")).mapToInt(Integer::parseInt).toArray();
-        return (arr[0] * 12 * 28) + (arr[1] * 28) + (arr[2]);
+    private int dayToInt(String day) {
+        int[] arr = Arrays.stream(day.split("\\.")).mapToInt(Integer::parseInt).toArray();
+        return arr[0]*12*28 + arr[1]*28 + arr[2];
     }
 }
