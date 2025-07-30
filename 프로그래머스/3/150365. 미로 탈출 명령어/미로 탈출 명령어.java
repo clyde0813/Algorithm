c9c5c9759d71a1375a567f3675fd719a12608b77
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-30 23:31:40 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.08 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,46 +1,46 @@
+import java.util.*;
+
 class Solution {
+    private int n, m, c, r, k;
     private int[][] dirs = {{1,0},{0,-1},{0,1},{-1,0}};
-    private String[] commands = {"d","l","r","u"};
-    private int n, m, r, c, k;
+    private String[] commands = {"d", "l", "r", "u"};
     
-    public String solution(int n, int m, int y, int x, int c, int r, int k) {
-        if((Math.abs(y-c)+Math.abs(x-r))>k ||
-           (k-(Math.abs(y-c)+Math.abs(x-r)))%2==1) return "impossible";
-        
+    public String solution(int n, int m, int y, int x, int c, int r, int k) {        
         this.n = n;
         this.m = m;
-        this.r = r;
         this.c = c;
+        this.r = r;
         this.k = k;
-        
-        StringBuilder sb = new StringBuilder();
-        dfs(y, x, sb);
-        
-        return sb.toString();
+            
+        String answer = dfs(y, x, new StringBuilder());
+        return (answer==null) ? "impossible" : answer;
     }
     
-    private boolean dfs(int y, int x, StringBuilder route) {
-        int remain = k - route.length();
-        if((Math.abs(y-c)+Math.abs(x-r)) > remain ||
-           (remain-(Math.abs(y-c)+Math.abs(x-r)))%2==1) return false;
-        
-        if(route.length()==k && y==c && x==r) return true;
+    private String dfs(int y, int x, StringBuilder route) {
+        if(!isAvailable(y, x, k-route.length())) return null;
+        if(route.length()==k && y==c && x==r) return route.toString();
         
         for(int i=0; i<4; i++) {
             int[] dir = dirs[i];
-            String command = commands[i];
             int dy = y+dir[0], dx = x+dir[1];
             if(!isInBound(dy, dx)) continue;
-            
-            route.append(command);
-            if(dfs(dy, dx, route)) return true;
+
+            String result = dfs(dy, dx, route.append(commands[i]));
+            if(result != null) return result;
             route.deleteCharAt(route.length()-1);
         }
         
-        return false;
+        return null;
     }
     
     private boolean isInBound(int y, int x) {
-        return m>=x && x>=1 && n>=y && y>=1;
+        return n>=y && y>=1 && m>=x && x>=1;
+    }
+    
+    private boolean isAvailable(int y, int x, int remain) {
+        return (
+            (Math.abs(y-c)+Math.abs(x-r)) <= remain &&
+            (remain - (Math.abs(y-c)+Math.abs(x-r))) % 2 == 0
+        );
     }
 }
