d46f19dd0c20f7ef6b7b5d0b273e858a50acf06d
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-07-31 20:55:24 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 3.13 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -2,17 +2,14 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] food) {
-        StringBuilder sb = new StringBuilder();
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i=1; i<food.length; i++) {
+        StringBuilder sb = new StringBuilder("0");
+
+        for(int i=food.length-1; i>=1; i--) {
             for(int j=0; j<food[i]/2; j++) {
-                list.add(i);
+                sb.insert(0, i);
                 sb.append(i);
             }
         }
-        sb.append(0);
-        for(int i=list.size()-1; i>=0; i--) sb.append(list.get(i));
         
         return sb.toString();
     }
