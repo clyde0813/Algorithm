0fb26ba406ef24ccdb9e5f1086d87799b895d780
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-08-01 23:11:06 +0900

    [level 1] Title: 햄버거 만들기, Time: 36.61 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] ingredient) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int answer = 0;
+        
+        for(int i : ingredient) {
+            if(stack.isEmpty() && i==1) stack.offer(1);
+            else if(!stack.isEmpty()) {
+                if(stack.peekLast()==3 && i==1) {
+                    answer++;
+                    for(int j=0; j<3; j++) stack.pollLast();
+                }
+                else if(i==1) stack.offer(1);
+                else if(stack.peekLast()==i-1) stack.offer(i);
+                else stack.clear();
+            }
+        }
+        
+        return answer;
+    }
+}
