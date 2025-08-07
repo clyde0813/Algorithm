30c346b8eb7202ce128398c38b22ca0d44787308
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-08-07 20:24:44 +0900

    [level 3] Title: 섬 연결하기, Time: 0.65 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/README.md
+++ 프로그래머스/3/42861. 섬 연결하기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 섬 연결하기 - 42861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42861) 
 
 ### 성능 요약
 
-메모리: 77.8 MB, 시간: 0.92 ms
+메모리: 80.1 MB, 시간: 0.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 12일 22:58:25
+2025년 08월 07일 20:24:39
 
 ### 문제 설명
 
--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -4,17 +4,14 @@ class Solution {
     private int[] parent;
     
     public int solution(int n, int[][] costs) {
-        if(n==1) return 0;
-        
         this.parent = new int[n];
         for(int i=0; i<n; i++) parent[i] = i;
         
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+        Arrays.sort(costs, (c1, c2) -> c1[2]-c2[2]);
         
         int answer = 0;
         for(int[] cost : costs) {
-            if(find(cost[0]) == find(cost[1])) continue;
-            
+            if(find(cost[0])==find(cost[1])) continue;
             union(cost[0], cost[1]);
             answer += cost[2];
         }
@@ -23,11 +20,11 @@ class Solution {
     }
     
     private int find(int node) {
-        if(parent[node] != node) parent[node] = find(parent[node]);
+        if(parent[node]!=node) parent[node] = find(parent[node]);
         return parent[node];
     }
     
     private void union(int node1, int node2) {
-        parent[find(node1)] = find(node2);        
+        parent[find(node1)] = find(node2);
     }
 }
