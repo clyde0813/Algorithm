9f81b8ee7ae028d796453d3b7e3628ea30281e13
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-08-13 22:53:30 +0900

    [level 2] Title: [1차] 캐시, Time: 37.50 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,26 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(int cacheSize, String[] cities) {
         if(cacheSize==0) return cities.length * 5;
-        
-        Deque<String> LRU = new ArrayDeque<>();
-        Set<String> cache = new HashSet<>();
+        LinkedHashMap<String, String> cache = new LinkedHashMap<>(cacheSize, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(final Map.Entry eldest) {
+                return size() > cacheSize;
+            }
+        };
         
         int answer = 0;
         for(String city : cities) {
             city = city.toLowerCase();
-            if(cache.contains(city)) {
-                answer += 1;
-                LRU.remove(city);
-                LRU.offerFirst(city);
-            }
-            else {
-                if(cache.size() == cacheSize) cache.remove(LRU.pollLast());
-                answer += 5;
-                cache.add(city);
-                LRU.offerFirst(city);
-            }
+            answer += (cache.containsKey(city)) ? 1 : 5;
+            cache.put(city, null);
         }
-        
         return answer;
     }
 }
