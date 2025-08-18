90fbee3ce6092f0eb8e0a42ca1e1985aea60b6ec
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-08-18 23:43:34 +0900

    [level 2] Title: [3차] n진수 게임, Time: 8.78 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,38 +1,12 @@
-import java.util.*;
-
 class Solution {
     public String solution(int n, int t, int m, int p) {
-        int len = ((t-1)*m) + p;
-        char[] arr = new char[len];
+        StringBuilder base = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
         
-        int idx = 0;
-        int num = 0;
-        while(idx<len) {  
-            char[] converted = convertToBase(n, num++).toCharArray();
-
-            for(int i=0; i<converted.length; i++) {
-                if(idx >= len) break;
-                arr[idx++] = converted[i];
-            }
-        }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i=p-1; i<len; i+=m) sb.append(arr[i]);
+        int number = 0;
+        while(base.length() < m*t) base.append(Integer.toString(number++, n));
+        for(int i=p-1; i<m*t; i+=m) answer.append(base.charAt(i));
         
-        return sb.toString();
-    }
-    
-    private String convertToBase(int n, int number) {
-        if(number == 0) return "0";
-        StringBuilder sb = new StringBuilder();
-        
-        while(number >= 1) {
-            int digit = number % n;
-            if(digit >= 10) sb.insert(0, (char) ('A' + (digit - 10)));
-            if(digit < 10) sb.insert(0, digit);
-            number = number / n;
-        }
-        
-        return sb.toString();
+        return answer.toString().toUpperCase();
     }
 }
