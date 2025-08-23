12ab3225f7f74bd95cced8fc1e6acf01fb1e4980
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-08-23 12:49:53 +0900

    [level 2] Title: 택배상자, Time: 35.34 ms, Memory: 144 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] order) {
-        Deque<Integer> sub = new ArrayDeque<>();
-        for(int i=1; i<order[0]; i++) sub.offerFirst(i);
+        Deque<Integer> stack = new ArrayDeque<>();        
+        int nextBox = 1;
+        int answer = 0;
         
-        int top = order[0]+1;
-        int answer = 1;
-        for(int i=1; i<order.length; i++) {
-            int o = order[i];
-            if(o == top) top += 1;
-            else if(!sub.isEmpty() && o == sub.peek()) sub.pollFirst();
-            else if(o > top) {
-                for(int j=top; j<o; j++) sub.offerFirst(j);
-                top = o+1;
+        for(int target : order) {
+            if(target == nextBox) {
+                nextBox++;
+            }
+            else if(!stack.isEmpty() && target == stack.peek()) stack.pop();
+            else if(target > nextBox) {
+                for(int j=nextBox; j<target; j++) stack.push(j);
+                nextBox = target + 1;
             }
             else break;
             answer++;
