66ba02786c63534e271be27cb7f96d1167c82b07
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-08-24 09:32:00 +0900

    [level 2] Title: 스킬트리, Time: 0.04 ms, Memory: 88.6 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -2,20 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        Map<Character, Integer> map = new HashMap<>();
-        for(int i=0; i<skill.length(); i++) map.put(skill.charAt(i), i);
-
         int answer = 0;
         for(String s : skill_trees) {
             int idx = 0;
             for(int i=0; i<s.length(); i++) {   
-                char c = s.charAt(i);
-                if(map.containsKey(c) && map.get(c) != idx) break;
-                if(map.containsKey(c) && map.get(c) == idx) idx++;
+                int c = skill.indexOf(s.charAt(i));
+                if(c != -1 && c != idx) break;
+                if(c == idx) idx++;
                 if(i == s.length()-1) answer++;
             }
         }
-        
         return answer;
     }
 }
