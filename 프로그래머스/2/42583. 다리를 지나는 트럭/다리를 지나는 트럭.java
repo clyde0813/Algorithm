06eec83506f642df3d07e9294757fe005e43844f
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-09-21 16:35:40 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 1.32 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -4,20 +4,22 @@ class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         Deque<int[]> deque = new ArrayDeque<>();
                 
-        int time = 0, idx = 0, sum = 0, count = 0;
+        int time = 0, idx = 0, onBridge = 0, n = truck_weights.length;
         
-        while(count < truck_weights.length) {       
+        while(idx < n || !deque.isEmpty()) {       
             time++;
             
             if(!deque.isEmpty() && deque.peek()[1] == time) {
-                sum -= deque.poll()[0];
-                count++;
+                onBridge -= deque.poll()[0];
             }
             
-            if(idx < truck_weights.length && sum + truck_weights[idx] <= weight) {
-                sum += truck_weights[idx];
+            if(idx < n && onBridge + truck_weights[idx] <= weight) {
+                onBridge += truck_weights[idx];
                 deque.offer(new int[]{truck_weights[idx], time + bridge_length});
                 idx++;
+            }
+            else if(idx < n && !deque.isEmpty()) {
+                time = deque.peek()[1] - 1;    
             }
         }
         
