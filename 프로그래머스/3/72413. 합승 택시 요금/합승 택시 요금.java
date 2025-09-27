fa7cc8cb12905890bc1ab5d57cb9cc881f298d58
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-09-27 16:19:15 +0900

    [level 3] Title: 합승 택시 요금, Time: 91.13 ms, Memory: 70.1 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -41,22 +41,26 @@ class Solution {
     private int[] dijkstra(int start) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((i1, i2) -> Integer.compare(i1[1], i2[1]));
         
-        boolean[] visited = new boolean[n+1];
         int[] result = new int[n+1];
         
         Arrays.fill(result, Integer.MAX_VALUE);
+        result[start] = 0;
         pq.offer(new int[]{start, 0});
         
         while(!pq.isEmpty()) {
             int[] node = pq.poll();
             int destination = node[0];
-            int cost = node[1];
-                        
-            if(result[destination] <= cost) continue;
+            int cost = node[1];   
             
-            result[destination] = cost;
-            for(int[] route : map.get(destination)) {                
-                pq.offer(new int[]{route[0], cost + route[1]});
+            if(!map.containsKey(destination)) continue;
+            
+            for(int[] route : map.get(destination)) {  
+                int next = route[0];
+                int nextCost = route[1];
+                if(result[next] <= cost + nextCost) continue;
+                
+                result[next] = cost + nextCost; 
+                pq.offer(new int[]{next, result[next]});
             }
         }
         
