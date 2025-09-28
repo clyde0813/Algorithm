2c81f5f856bede8f522db4a601e5cedc4291d4c7
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-09-28 18:31:18 +0900

    [level 2] Title: 삼각 달팽이, Time: 9.03 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -3,21 +3,20 @@ class Solution {
         int size = n * (n+1) / 2;
         int[] answer = new int[size];
         
-        int x = -1, y = 0;
+        int[][] dirs = {{1,0}, {0,1}, {-1,-1}};
+        int dIdx = 0;
         int num = 1;
-        
-        int[][] dir = {{1,0}, {0,1}, {-1,-1}};
-        int d = 0;
+        int x = -1, y = 0;
         
         for(int step=n; step>0; step--) {
             for(int i=0; i<step; i++) {
-                x += dir[d][0];
-                y += dir[d][1];
+                x += dirs[dIdx][0];
+                y += dirs[dIdx][1];
                 
                 int idx = x * (x+1) / 2 + y;
                 answer[idx] = num++;
             }
-            d = (d+1) % 3;
+            dIdx = (dIdx+1) % 3;
         }
         
         return answer;
