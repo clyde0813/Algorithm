4cc85e19c5a02692d9eefc7ffe4fe68c66614bc6
Clyde <76196439+clyde0813@users.noreply.github.com>
2025-10-10 14:42:23 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.03 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int n = num_list.length;
+        int[] answer = new int[n];
+        
+        for(int i=n-1; i>=0; i--) {
+            answer[n-i-1] = num_list[i]; 
+        }
+        
+        return answer;
+    }
+}
